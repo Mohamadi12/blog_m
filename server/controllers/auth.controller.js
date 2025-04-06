8f88153cdf2f078212dc7452338e719925076a03
Nana Mohamadi <adilnana78@gmail.com>
2025-04-06 21:43:35 +0000

    Add a middleware and a function to handle errors

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -13,7 +13,7 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json("Please fill all fields");
+    next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -23,6 +23,11 @@ export const signup = async (req, res) => {
     email,
     password: hashedPassword,
   });
-  await newUser.save();
-  res.json("Signup successful");
+
+  try {
+    await newUser.save();
+    res.json("Signup successful");
+  } catch (error) {
+    next(error);
+  }
 };
--- server/index.js
+++ server/index.js
@@ -1,28 +1,37 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('MongoDb is connected');
+    console.log("MongoDb is connected");
   })
   .catch((err) => {
     console.log(err);
   });
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//Routes 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
-
+//Routes
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+  console.log("Server is running on port 3000");
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
