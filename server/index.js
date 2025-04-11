a8f6ff8c6bf712bdeb87996507ce7890810e1a26
Nana Mohamadi <adilnana78@gmail.com>
2025-04-11 00:35:54 +0000

    Comment part

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -24,6 +25,7 @@ app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
